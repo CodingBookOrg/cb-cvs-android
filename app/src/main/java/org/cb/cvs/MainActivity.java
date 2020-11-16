507ee6c6e9bc495927063de0a1544b9db8d97c02
orhan <orhanu2020@gmail.com>
2020-11-16 12:33:43 -0500

    Initial Application

---
+++ app/src/main/java/org/cb/cvs/MainActivity.java
@@ -0,0 +1,14 @@
+package org.cb.cvs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
